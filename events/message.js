4bf10cf1b0e65d65da0a7ccc15296bba1ac901b7
Felix <felixbeuving@live.nl>
2019-10-08 22:54:31 +0200

    v0.2: commands ability & eval command

---
+++ events/message.js
@@ -0,0 +1,19 @@
+﻿exports.run = (client, message) => {
+    if(message.author.bot) return;
+
+    const args = message.content.trim().split(/ +/g);
+
+    //Check if I am tagged
+    if(args.shift() !== `<@${client.user.id}>`) return;
+    client.logger.log(`Message received: ${message.content} from ${message.author.username}`);
+
+    if(args.length == 0) return(message.reply(`Please tell me what to do! :)`));
+
+    //Check if command is valid
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command);
+    if(!cmd) return;
+
+    client.logger.log(`${message.author.username} ran command "${command}" with args: ${args}`, "cmd");
+    cmd.run(client, message, args);
+};
